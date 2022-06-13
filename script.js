520771244a8dbe606740d4321e0d8708d1fbb77b
April Hughes <dreydain@gmail.com>
2022-06-13 12:22:55 -0700

    completed lecture 71 & 72

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message');
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
