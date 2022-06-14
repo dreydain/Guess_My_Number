02953c1d4ac116d1f33d1318489bc16616c40abb
April Hughes <dreydain@gmail.com>
2022-06-14 12:22:50 -0700

    lecture 76 Coding Challenge 1. Reseting the game completed

--- script.js
+++ script.js
@@ -13,10 +13,9 @@ console.log(document.querySelector('.guess').value);
 
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
 		// When Player Wins
 	} else if (guess === secretNumber) {
 		document.querySelector('.message').textContent = '🎉 Correct Number!';
+		document.querySelector('.number').textContent = secretNumber;
+
 		document.querySelector('body').style.backgroundColor = '#60b347';
 
 		document.querySelector('.number').style.width = '30rem';
@@ -55,4 +56,16 @@ document.querySelector('.check').addEventListener('click', function () {
 			document.querySelector('.score').textContent = 0;
 		}
 	}
+});
+
+// Resetting the Game - Coding Challenge #1
+document.querySelector('.again').addEventListener('click', function () {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+	document.querySelector('.message').textContent = 'Start guessing...';
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.number').textContent = '?';
+	document.querySelector('.guess').value = '';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
 });
